3b9522fe4c2a4c192ac7578d0fea01e901709d91
Yo-Rock <luonggiahieu2700@gmail.com>
2022-09-11 20:03:29 +0700

    commit

--- client/src/components/user/CreatePost/index.jsx
+++ client/src/components/user/CreatePost/index.jsx
@@ -1,6 +1,6 @@
 function CreatePost({ user, setVisible, profile }) {
   return (
-    <div className={`createPost ${profile ? "mt-3" : ""}`}>
+    <div className={`createPost ${profile ? 'mt-3' : ''}`}>
       <div className="createPost_header">
         <img src={user?.picture} alt="" />
         <div className="open_post hover2" onClick={() => setVisible(true)}>
