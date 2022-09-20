8d05115031daa4ea1da42e351968a93172343663
Yo-Rock <luonggiahieu2700@gmail.com>
2022-09-20 16:33:10 +0700

    remove gitignore files

--- server/helpers/generateCode.js
+++ server/helpers/generateCode.js
@@ -1,6 +1,6 @@
 function generateCode(length) {
-  let code = "";
-  let schema = "0123456789";
+  let code = '';
+  let schema = '0123456789';
 
   for (let i = 0; i < length; i++) {
     code += schema.charAt(Math.floor(Math.random() * schema.length));
